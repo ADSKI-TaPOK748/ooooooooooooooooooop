a22b5768137eceaddba55a73c3141a98475bfdfa
ADSKI-TaPOK748 <XE@XE4943>
2025-02-23 15:31:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Common;
+
+ClassCat murk = new ClassCat();
+ClassCat moshka = new ClassCat();
+
+murk.age = 5;
+murk.name = "Murk";
+murk.color = "white";
+murk.poroda = "neznay";
+murk.height = 30;
+
+moshka.age = 6;
+moshka.name = "moshka";
+moshka.color = "black";
+moshka.poroda = "kokayata";
+moshka.height = 35;
